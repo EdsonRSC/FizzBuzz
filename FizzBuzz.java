56a25816be936de43e86e6e4506c895baa3ce67d
edson <edsonrubb@gmail.com>
2024-01-06 17:56:31 -0600

    If para FizzBuzz

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,5 +1,11 @@
 public class FizzBuzz {
     public static void main(String args[]){
+        //Se crea el for para ir contando del uno al cien
+        for (int i = 0; i <= 100; i++) {
+            if(i%5==0 && i%3==0){
+                System.out.println("Fizz Buzz");
+            }
         
+        }
     }
 }
