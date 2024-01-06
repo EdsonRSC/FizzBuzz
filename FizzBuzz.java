c2504a2672b25ae3faee8ee39f818535e55a8931
edson <edsonrubb@gmail.com>
2024-01-06 17:58:15 -0600

    If para Fizz

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -2,8 +2,11 @@ public class FizzBuzz {
     public static void main(String args[]){
         //Se crea el for para ir contando del uno al cien
         for (int i = 0; i <= 100; i++) {
+            //Se crean los if para hacer las sustituciones
             if(i%5==0 && i%3==0){
                 System.out.println("Fizz Buzz");
+            }else if(i%3==0){
+                System.out.println("Fizz");
             }
         
         }
