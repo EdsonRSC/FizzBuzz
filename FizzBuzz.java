1f36bb34ee90a3e64b696c88956b3d16549ddf46
edson <edsonrubb@gmail.com>
2024-01-06 18:21:08 -0600

    Correccion de i

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,7 +1,7 @@
 public class FizzBuzz {
     public static void main(String args[]){
         //Se crea el for para ir contando del uno al cien
-        for (int i = 0; i <= 100; i++) {
+        for (int i = 1; i <= 100; i++) {
             //Se crean los if para hacer las sustituciones
             if(i%5==0 && i%3==0){
                 System.out.println("Fizz Buzz");
